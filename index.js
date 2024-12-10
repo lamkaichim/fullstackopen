d977416b53385c86d4045c8522cd382cc2daa65b
stephenlkc <stephenlkc@github.com>
2024-12-10 09:51:04 +0800

    Update feature for ex3.9

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(morgan('tiny'))
+app.use(cors())
+app.use(express.static('dist'))
+
+morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '');
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = [
   {
@@ -62,7 +71,37 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const generateId = () => {
+  return Math.floor(Math.random() * 1000000); // 生成一个 0 到 999999 的随机整数
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number is missing'
+    })
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
